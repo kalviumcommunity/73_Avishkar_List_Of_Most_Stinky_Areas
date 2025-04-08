6f93da06393d64d69f4eeaabcf309e50858ab423
Avishkar-s73 <avishkar.durgade.s73@kalvium.community>
2025-04-08 10:41:40 +0530

    dbSchema

---
+++ backend/model/index.js
@@ -0,0 +1,5 @@
+import User from "./user.js";
+import StinkyArea from "./StinkyArea.js";
+import Comment from "./Comment.js";
+
+export { User, StinkyArea, Comment };
---
+++ backend/model/user.js
@@ -0,0 +1,77 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "Username is required"],
+      unique: true,
+      trim: true,
+      minlength: [3, "Username must be at least 3 characters long"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      trim: true,
+      lowercase: true,
+      match: [/^\S+@\S+\.\S+$/, "Please enter a valid email address"],
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minlength: [6, "Password must be at least 6 characters long"],
+    },
+    profilePicture: {
+      type: String,
+      default: "",
+    },
+    bio: {
+      type: String,
+      default: "",
+      maxlength: [200, "Bio cannot exceed 200 characters"],
+    },
+    reportedAreas: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "StinkyArea",
+      },
+    ],
+    votedAreas: [
+      {
+        area: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "StinkyArea",
+        },
+        vote: {
+          type: Number,
+          enum: [-1, 1], // Downvote or upvote
+        },
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+// Hash password before saving
+UserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Method to compare password
+UserSchema.methods.comparePassword = async function (candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
+
+const User = mongoose.model("User", UserSchema);
+
+export default User;
